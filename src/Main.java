fec2b9aa0e55e2918a2986a4c6018e7112c44a7f
Anastasia <n41111215@mail.ru>
2020-05-04 23:16:35 +0300

    Добавила код для миль

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long price=3000;
+                long bonus=price/20;
+                System.out.println(bonus);
+
+    }
+}
